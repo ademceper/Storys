67a6db72e2c54cdcd57ec44bad2b8e8471c7bfb9
Adem Çeper <120774440+ademceper@users.noreply.github.com>
2023-07-22 19:14:39 +0300

    Stroys
    Stroys

---
+++ Story/Models/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Story.Models;
+
+public class RegisterViewModel
+{
+    [Display(Name = "Ad Soyad")]
+    [Required(ErrorMessage = "{0} alanı boş bırakılamaz!")]
+    [DataType(DataType.Text)]
+    public required string Name { get; set; }
+
+
+    [Display(Name = "Email")]
+    [Required(ErrorMessage = "{0} alanı boş bırakılamaz!")]
+    [DataType(DataType.EmailAddress)]
+    public required string Email { get; set; }
+
+  
+
+   
+    public string? ReturnUrl { get; set; }
+}
